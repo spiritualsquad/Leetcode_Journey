d02457e91530ce1ada0ed63ed147b12ac44e1eaa
spiritualsquad <dondikshu@gmail.com>
2024-02-22 12:43:27 -0500

    C sharp

---
+++ findTheTownJudge/Solution.cs
@@ -0,0 +1,16 @@
+public class Solution {
+    public int FindJudge(int n, int[][] trust) {
+        int[] count = new int[n+1];
+        for(int i = 0; i<trust.Length;i++){
+            count[trust[i][0]]--;
+            count[trust[i][1]]++;
+        }
+        for(int i =1;i<=n;i++){
+            if(count[i]==n-1){
+                return i;
+            }
+        }
+        return -1;
+
+    }
+}
