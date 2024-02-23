222dfab1b49e90c22a231a67cfa5b4bd556e30dc
spiritualsquad <dondikshu@gmail.com>
2024-02-23 13:38:22 -0500

    Simple changes Done

--- cheapestFlightWithinKStops/Solution.java
+++ cheapestFlightWithinKStops/Solution.java
@@ -1,20 +1,19 @@
-//doesnt work
 class Solution {
     public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
         int[] prev= new int[n];
         Arrays.fill(prev,(int)1e8);
         prev[src]=0;
-        for(int i = 0;i<k;i++){
+        for(int i = 0;i<=k;i++){
             int[] curr = new int[n];
-            for(int j = 0;j<k;j++){
+            for(int j = 0;j<n;j++){
                 curr[j] = prev[j];
             }
-            for(int[] flight: flights){
+            for(int[] flight:flights){
                 int u = flight[0];
                 int v = flight[1];
                 int w = flight[2];
-                if(curr[v]>prev[v]+w){
-                    curr[v] = prev[v]+w;
+                if(curr[v]>prev[u]+w){
+                    curr[v] = prev[u]+w;
                 }
             }
             prev = curr;
