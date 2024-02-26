d97eec5a0ada02d0b65aeb195ab7ac6d41e07d24
spiritualsquad <dondikshu@gmail.com>
2024-02-25 20:10:05 -0500

    new

--- maxSubArray/Solution.java
+++ maxSubArray/Solution.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class Solution {
-}
+public class Solution {
+
+        public static void main(String[] args) {
+            int[] a = {-1,4,-6,8};
+            int maxSum = a[0];
+            int currentSum = a[0];
+            for(int i =1;i<a.length;i++){
+                if(a[i]>currentSum+a[i]){
+                    currentSum =a[i];
+                }else{
+                    currentSum = currentSum +a[i];
+                }
+                if(currentSum>maxSum){
+                    maxSum=currentSum;
+                }
+
+            }
+            System.out.println("Max sum="+maxSum);
+        }
+    }
